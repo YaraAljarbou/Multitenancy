e9500839172b1b1c4cc7808a9d7c6d0d6fce3a95
Tareq-Assiri <tareqassiri2000@gmail.com>
2023-10-12 11:13:52 +0300

    latest async with timeout handling

--- src/main/java/com/yara/multitenancy/config/AsyncConfiguration.java
+++ src/main/java/com/yara/multitenancy/config/AsyncConfiguration.java
@@ -1,14 +1,11 @@
 package com.yara.multitenancy.config;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.ApplicationContextAware;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.Executor;
